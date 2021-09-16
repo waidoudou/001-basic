59f13a5427eaf11bd451317b89f0affdd7b44b5a
waidou <weitao3987@163.com>
2021-09-16 10:57:32 +0800

    second commit

--- src/main/java/com/entity/Student.java
+++ src/main/java/com/entity/Student.java
@@ -3,5 +3,6 @@ package com.entity;
 public class Student {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello git");
     }
 }
